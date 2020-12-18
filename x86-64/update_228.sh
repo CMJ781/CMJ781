d32b1786a67149d1e801dfb54b6cac17620c23de
CMJ781 <32243470+CMJ781@users.noreply.github.com>
2020-12-18 08:34:09 +0800

    Update update_228.sh

--- x86-64/update_228.sh
+++ x86-64/update_228.sh
@@ -7,6 +7,7 @@
 rm -rf ./package/lean/trojan
 rm -rf ./package/lean/v2ray
 rm -rf ./package/lean/v2ray-plugin
+rm -rf ./package/lean/xray
 
 rm -rf ./feeds/packages/net/smartdns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./package/diy/smartdns
