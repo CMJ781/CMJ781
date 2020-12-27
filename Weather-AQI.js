2566406414a4c3576e7efad47426ff01419b1fa7
CMJ781 <32243470+CMJ781@users.noreply.github.com>
2020-12-27 21:26:46 +0800

    Update Weather-AQI.js

--- Weather-AQI.js
+++ Weather-AQI.js
@@ -1,16 +1,4 @@
-// Developed by Hackl0us (https://github.com/hackl0us)
-
-// STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
 const aqicnToken = '75764fbcf9412cfac31ab0e641680f80d717b161'
-
-// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
-/*
-	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=https://raw.githubusercontent.com/CMJ781/CMJ781/master/Weather-AQI.js
-
-	[MITM]
-	hostname = weather-data.apple.com
-*/
 
 const AirQualityStandard = {
 	CN: 'HJ6332012.1',
