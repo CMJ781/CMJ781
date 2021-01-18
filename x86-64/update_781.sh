32bd1a816dfcb7364e79422ff06b048b7d4b0d3e
CMJ781 <32243470+CMJ781@users.noreply.github.com>
2021-01-18 15:15:30 +0800

    Update update_781.sh

--- x86-64/update_781.sh
+++ x86-64/update_781.sh
@@ -13,20 +13,23 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns ./package/diy
 rm -rf ./feeds/packages/net/mwan3 && \
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/mwan3 ./feeds/packages/net/mwan3
 
+rm -rf ./feeds/packages/net/https-dns-proxy
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy ./feeds/packages/net/https-dns-proxy
+
 rm -rf ./package/diy/mwan3
 rm -rf ./package/lean/autocore
 rm -rf ./package/lean/default-settings
 
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
-sed -i 's / 网络存储 / 存储 /g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
-sed -i 's / 带宽监控 / 监控 /g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-sed -i 's/Turbo ACC 网络加速 / 网络加速 /g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
-#sed -i's/Turbo ACC 网络加速 / 网络加速 /g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
-sed -i 's / 解锁网易云灰色歌曲 / 解锁灰色歌曲 /g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
-#sed -i's / 家庭云 //g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
-sed -i 's / 实时流量监测 / 流量 /g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
-#sed -i's / 家庭云 //g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
+sed -i 's/网络存储/存储 /g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
+sed -i 's/带宽监控/监控 /g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+sed -i 's/Turbo ACC 网络加速/网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
+#sed -i's/Turbo ACC 网络加速/网络加速 /g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
+sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲 /g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+#sed -i's/家庭云//g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
+sed -i 's/实时流量监测/流量/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
+#sed -i's/家庭云//g' package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
 #sed -i's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 #sed -i's/cbi ("qbittorrent"),_("qBittorrent")/cbi ("qbittorrent"),_("BT 下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
 #sed -i's/"aMule 设置"/"电驴下载"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
