4bbe251573884b4bc62bd92eef94d0ef1d3c53ee
CMJ781 <32243470+CMJ781@users.noreply.github.com>
2021-01-25 08:28:06 +0800

    Update update_228.sh

--- x86-64/update_228.sh
+++ x86-64/update_228.sh
@@ -56,11 +56,11 @@ echo  'CONFIG_NVME_FC=y' >> ./package/target/linux/x86/config-5.4
 echo  'CONFIG_NVME_MULTIPATH=y' >> ./package/target/linux/x86/config-5.4
 echo  'CONFIG_NVME_TCP=y' >> ./package/target/linux/x86/config-5.4
 
-#git clone https://github.com/garypang13/luci-app-bypass.git package/diy/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass.git package/diy/luci-app-bypass
 #git clone https://github.com/garypang13/luci-app-dnsfilter.git package/diy/luci-app-dnsfilter
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/diy/luci-app-openclash
 git clone https://github.com/xiaorouji/openwrt-passwall package/diy1
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan ./package/diy/luci-app-serverchan
-#svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
+svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
 
 ./scripts/feeds update -i
